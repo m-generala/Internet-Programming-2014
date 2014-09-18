4f04f12a869c452c3bb2a5a5b726e264ee9c2468
m-generala <marto@ubuntu.(none)>
2014-09-18 09:20:29 +0300

    Gettin better

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,25 @@
 public class Main {
 	public Main(int a,String b){
 		String brand=b;
-		int speed=a;
-		int topspeed=0;
-		if (topspeed < speed){
-			topspeed=speed;
-		}
+		int speed=a;		
 		System.out.println(brand + "s' current speed is " + speed + "Km/h");
-		System.out.println(brand + "s' top speed is " + (topspeed + 15));
 	}
 	public static void main(String[] args) {
-	for(int i=100;i<260;i+=20){
-		
-		Main Mercedes = new Main(i,"Mercedes 190E");
+		int check = 0;
+		System.out.println("Flying start at 130Km/h");
+	for(int i=130;i<260;i+=20){
+		check+=1;
+		System.out.println("-----------Checkpoint " + check + "-------------");
+		if(i<190){
+			Main Mercedes = new Main(i,"Mercedes 190E");
+		}else{
+			Main Mercedes = new Main(i-10,"Mercedes 190E");
+		}
 		Main BMW = new Main(i+10,"BMW M3 E30");
-
+		
 	}
+		System.out.println("Aaaaannndd FINISH");
+	
 	
 	}
 
