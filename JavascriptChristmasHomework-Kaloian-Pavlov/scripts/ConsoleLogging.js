1f4290c4af8543c26536df03041dbfe0e4f898cf
Kaloyan <marti_world@abv.bg>
2014-12-30 05:20:28 +0200

    Logging tasks

---
+++ JavascriptChristmasHomework-Kaloian-Pavlov/scripts/ConsoleLogging.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    var paragraphInCol1 = $('#col1 p');
+    console.log(paragraphInCol1.html());
+
+    var tuLink = $('#footer a:first-child');
+    console.log(tuLink.attr('title'));
+});
