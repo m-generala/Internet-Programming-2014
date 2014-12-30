4d0303290a76233b1bed0d11e03106cdb3009bae
Kaloyan <kaloian.pavlov@abv.bg>
2014-12-30 15:15:34 +0200

    Click handlers

---
+++ JavascriptChristmasHomework-Kaloian-Pavlov/scripts/clickHandlers.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../libs/jquery-1.3.2.min.js" />
+$(document).ready(function () {
+    function addOnClickTopMenuLink() {
+        var linkElement = $('#menu-top-level-menu a:last-child');
+        linkElement.click(function () {
+            var col1 = $('#col1'),
+                col2 = $('#col2'),
+                tmp = col1.html();
+
+            col1.html(col2.html());
+            col2.html(tmp);
+        });
+    }
+
+    addOnClickTopMenuLink();
+});
