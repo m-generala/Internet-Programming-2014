32dbdfe7254be4044ac4dcacfe68d6ce7cd1b4fd
Kaloyan <marti_world@abv.bg>
2014-12-30 05:41:23 +0200

    Dom manipulation

--- JavascriptChristmasHomework-Kaloian-Pavlov/scripts/ConsoleLogging.js
+++ JavascriptChristmasHomework-Kaloian-Pavlov/scripts/ConsoleLogging.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
-    var paragraphInCol1 = $('#col1 p');
-    console.log(paragraphInCol1.html());
+    var paragraphInCol1 = $('#col1 p'),
+        tuLink = $('#footer a:first-child');
 
-    var tuLink = $('#footer a:first-child');
+    console.log(paragraphInCol1.html());
     console.log(tuLink.attr('title'));
 });
---
+++ JavascriptChristmasHomework-Kaloian-Pavlov/scripts/domManipulation.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="../libs/jquery-1.3.2.min.js" />
+$(document).ready(function () {
+    function addButton() {
+        var unorderedList = $('#menu-top-level-menu'),
+            listElement = $('<li>'),
+            linkElement = $('<a>');
+
+        linkElement.html('new button');
+        listElement.append(linkElement);
+        unorderedList.append(listElement);
+    }
+
+    //prepends
+    function addDivToFooter() {
+        var footer = $('#footer'),
+            div = $('<div>'),
+            input = $('<input>'),
+            button = $('<button>'),
+            ul = $('<ul>');
+
+        input.attr('id', 'textinput');
+        ul.attr('id', 'posts');
+        div.attr('id', 'dynamiccontent');
+
+        button.html('I AM BUTTON');
+
+        div.append(input);
+        div.append(button);
+        div.append(ul);
+        footer.prepend(div);
+    }
+
+    addButton();
+    addDivToFooter();
+});
