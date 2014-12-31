657a947a757f940fbc30eb6a1fb82ad6f65a3196
m-generala <marti_world@abv.bg>
2014-12-31 18:15:47 +0200

    Tasks 4 and 5

--- X-mas homework Martin Kuzev/hw.js
+++ X-mas homework Martin Kuzev/hw.js
@@ -1,9 +1,21 @@
-﻿$(document).ready(function () {
+﻿ $(document).ready(function () {
     "use strict"
     var a = $("a.tu");
     console.log(a.attr("title"));
+
     var col = $("#col1 p");
     console.log(col.html());
 
+    var unorderedList = $('#menu-top-level-menu'),
+            listElement = $('<li>'),
+            linkElement = $('<a>');
+     
+    linkElement.html('new button');
+    listElement.append(linkElement);
+    unorderedList.append(listElement);
 
+    var footer = $('#footer'),
+            div = $('<div>');
+            div.attr('id', 'dynamiccontent');
+            footer.prepend(div);
 });
